f3abc5e74291acf3919334730ef779aced599356
fitrinyc <novyanti.fitria04@gmail.com>
2024-05-27 11:00:51 +0700

    progress kedua

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aplikasi Perpustakaan</title>
+    <title>Aplikasi Perpustakaan V2</title>
     <style>
         body {
             font-family: Arial, sans-serif;
